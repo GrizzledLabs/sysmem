8a4740190479bcc53181ca0e3803621e430cdbbd
Tim Berberich <tmb@timberberich.com>
2019-08-23 09:13:28 -0400

    Updated code
    Code now uses 85% threshold

--- sysmem.py
+++ sysmem.py
@@ -4,9 +4,9 @@ Keeps track of your RAM and warns you if running 90% or more
 
 import psutil, time, datetime
 while True:
-	time.sleep(2)
+	time.sleep(5)
 	mem = psutil.virtual_memory()
-	if mem.percent > 90:
+	if mem.percent >= 85:
 		print(f'LOW RAM -- {mem.percent} --  {datetime.datetime.now().strftime("%H:%M:%S")}')
 	else:
 		print(f'{mem.percent} -- {datetime.datetime.now().strftime("%H:%M:%S")}')
