6c5526fe1339d8f5a720911b7512325b89233438
Tim Berberich <tmb@timberberich.com>
2019-08-23 09:35:04 -0400

    Code update
    Added % to the print statements.

--- sysmem.py
+++ sysmem.py
@@ -1,5 +1,5 @@
 '''
-Keeps track of your RAM and warns you if running 90% or more
+Keeps track of your RAM and warns you if running 85% or more in a while loop. Requires psutil.
 '''
 
 import psutil, time, datetime
@@ -7,6 +7,6 @@ while True:
 	time.sleep(5)
 	mem = psutil.virtual_memory()
 	if mem.percent >= 85:
-		print(f'LOW RAM -- {mem.percent} --  {datetime.datetime.now().strftime("%H:%M:%S")}')
+		print(f'LOW RAM -- {mem.percent}% --  {datetime.datetime.now().strftime("%H:%M:%S")}')
 	else:
-		print(f'{mem.percent} -- {datetime.datetime.now().strftime("%H:%M:%S")}')
+		print(f'{mem.percent}% -- {datetime.datetime.now().strftime("%H:%M:%S")}')
